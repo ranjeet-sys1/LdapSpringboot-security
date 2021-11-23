8b263e408e87e967c5e916c4afa9f6784ef9954e
ranjeet.suman <ranjeet.suman@ext.deposit-solutions.com>
2021-11-23 21:12:32 +0530

    successfully ldap configured

---
+++ src/test/java/com/app/LdapSpringSecurity/LdapSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.LdapSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

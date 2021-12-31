c5228da0289d8e1457932593fd75de9f83889da0
ranjeet.suman <ranjeet.suman@ext.deposit-solutions.com>
2021-12-31 13:35:07 +0530

    api added

--- src/main/java/com/app/LdapSpringSecurity/controller/HomeController.java
+++ src/main/java/com/app/LdapSpringSecurity/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.app.LdapSpringSecurity.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    public static final  Logger logger= LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/")
     public String index() {
@@ -13,6 +16,9 @@ public class HomeController {
     }
     @GetMapping("/open")
     public String getInfo(){
+        String name="ranjeet";
+        logger.info("LdapSecurity from info level : {}",name);
+        logger.debug("LdapSecurity from DEBUG level : {}",name);
         return "I am open book person";
     }
 

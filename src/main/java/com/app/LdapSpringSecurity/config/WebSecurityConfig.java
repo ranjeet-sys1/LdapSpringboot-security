5c15c35fdce1d561296da6f43c484d6a03a19507
ranjeet.suman <ranjeetsuman123@gmail.com>
2022-01-05 20:42:34 +0530

    Jwt token added

--- src/main/java/com/app/LdapSpringSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/app/LdapSpringSecurity/config/WebSecurityConfig.java
@@ -4,19 +4,27 @@ package com.app.LdapSpringSecurity.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable().csrf().disable();
         http
                 .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/open").authenticated()
+                .antMatchers("/getToken").authenticated()
                 .anyRequest().fullyAuthenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .httpBasic();
     }
 
     @Override
--- src/main/java/com/app/LdapSpringSecurity/controller/HomeController.java
+++ src/main/java/com/app/LdapSpringSecurity/controller/HomeController.java
@@ -1,13 +1,29 @@
 package com.app.LdapSpringSecurity.controller;
 
 
+import com.app.LdapSpringSecurity.model.TokenGeneration;
+import com.app.LdapSpringSecurity.utility.ResponseConstantIntegerValue;
+import com.app.LdapSpringSecurity.utility.ResponseConstantValue;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class HomeController {
+    
     public static final  Logger logger= LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/")
@@ -20,6 +36,24 @@ public class HomeController {
         logger.info("LdapSecurity from info level : {}",name);
         logger.debug("LdapSecurity from DEBUG level : {}",name);
         return "I am open book person";
+    }
+    @PostMapping("/getToken")
+    public ResponseEntity generateToken(@RequestBody TokenGeneration tokenGeneration){
+
+        List<String> message= new ArrayList<>();
+        String token = Jwts.builder()
+                .setId(tokenGeneration.getId())
+                .setSubject(tokenGeneration.getSubject())
+                .setIssuer("ranjeet")
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(10)))
+                .signWith(SignatureAlgorithm.HS256, Base64.getEncoder().encode(tokenGeneration.getSecretKey().getBytes())).compact();
+        message.add(token);
+        message.add(ResponseConstantValue.SUCCESS_MESSAGE);
+        message.add(String.valueOf(ResponseConstantIntegerValue.SUCCESS_RESPONSE));
+        return new ResponseEntity(message, HttpStatus.OK);
+
+
     }
 
 }

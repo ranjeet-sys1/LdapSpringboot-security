2d1c18fffbad19239c8abd5c86399f8d9f711053
ranjeet.suman <ranjeetsuman123@gmail.com>
2022-01-05 20:47:35 +0530

    Util classes and model class added

---
+++ src/main/java/com/app/LdapSpringSecurity/model/TokenGeneration.java
@@ -0,0 +1,31 @@
+package com.app.LdapSpringSecurity.model;
+
+public class TokenGeneration {
+    private String id;
+    private String subject;
+    private String secretKey;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
